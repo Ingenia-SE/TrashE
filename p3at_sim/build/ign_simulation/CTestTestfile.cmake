3ad410ea196d2fbf77c1b07903fc70118bdeed94
Pedro Soler <74536059+PedroSoler10@users.noreply.github.com>
2022-03-28 13:54:21 +0200

    Delete p3at_sim/build directory

--- p3at_sim/build/ign_simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pedro/TrashE/p3at_sim/src/ign_simulation
-# Build directory: /home/pedro/TrashE/p3at_sim/build/ign_simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
